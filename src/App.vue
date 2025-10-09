d199de6cab5edf6c563a7b1f6ab47221d2500bb5
Trabmini <tooktrab@icloud.com>
2025-10-09 14:58:16 -0400

    Int. Commit
    Int. Commit, non-factor

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Tecum from './components/pages/Tecum.vue';
+
+</script>
+
+<template>
+
+<Tecum />
+
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ src/components/pages/Tecum.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <section id="welcome">
+    <div class="benefits">
+        <h2>Complete this ratio</h2>
+        <div>
+            <p>✅ Sequere Viam Lucis, become disciplined. <strong>Train Like Legionaries</strong> </p>
+             <p>Crucifige Carnem — Esto Templum Dei </p>
+           
+        </div>
+
+    </div>
+    <div>
+        <h3>Regiment</h3>
+        <p>A hybrid endurance and strength system inspired by the physical training of Roman soldiers, designed to build <strong>full-body power, grit, and mental discipline.</strong></p>
+    </div>
+    </section>
+</template>
+
+<style scoped>
+ 
+</style>
